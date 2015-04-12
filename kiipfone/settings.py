e27e6bade6e7c1b47052a88d531f40f6168ce306
wgcv <gstavocevallos@gmail.com>
2015-04-12 13:44:52 -0500

    probar los archivos staticos

--- kiipfone/settings.py
+++ kiipfone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sitio',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,7 +81,7 @@ DATABASES = {
         'NAME': 'kiipfonedb',
 	'USER': 'kiipfone',
 	'PASSWORD': 'kiipf0ne!base',
-	'HOST': 'localhost',
+	'HOST': 'wgcv.me',
 	'PORT': '',
     }
 }
@@ -105,4 +106,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+MEDIA_ROOT = BASE_DIR + '/media/'
+TEMPLATE_DIRS = ( os.path.join(BASE_DIR, 'templates'), BASE_DIR,)
+LOGIN_URL = '/ingresar'
+LOGIN_REDIRECT_URL = "/"
--- kiipfone/urls.py
+++ kiipfone/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'kiipfone.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('sitio.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
