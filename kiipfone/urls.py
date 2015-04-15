9d17ebf04deb1aae06123851982b6985501f99cc
wgcv <gstavocevallos@gmail.com>
2015-04-15 18:37:37 -0500

    Se agrego iconos de redes sociales

--- kiipfone/urls.py
+++ kiipfone/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'kiipfone.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^blog/', include('blog.urls'STATIC_URL = "/static/"
     url(r'^', include('sitio.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
