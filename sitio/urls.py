164e342df83bb66b10e4b6f8ae8c6df909d50548
wgcv <gstavocevallos@gmail.com>
2015-04-27 22:33:53 -0500

    Se creo Contactanos

--- sitio/urls.py
+++ sitio/urls.py
@@ -6,5 +6,7 @@ from sitio import views
 urlpatterns = (
 	url(r'^$', 'sitio.views.home', name='home'),
 	url(r'^base/', 'sitio.views.base', name='base'),
+	url(r'^nosotros/', 'sitio.views.nosotros', name='nosotros'),
+	url(r'^contactanos/', 'sitio.views.contactanos', name='contactanos'),
 
     )
