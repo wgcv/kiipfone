9108a8a66dfeb042fdd3053bfd6ea404a940ea24
wgcv <gstavocevallos@gmail.com>
2015-05-07 09:37:38 -0500

    Se esta creando el seleccionar marca

--- solicitar/views.py
+++ solicitar/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def solicitar(request):
-    return render(request, 'solicitar.html' )
+	marcas = Marca.objects.all()
+    return render(request, 'solicitar.html' , marcas)
