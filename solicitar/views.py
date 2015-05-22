331eed33c3f1e9f5017e6ceb99ae58d13d2cf060
wgcv <gstavocevallos@gmail.com>
2015-05-21 23:49:11 -0500

    Version 1.0

--- solicitar/views.py
+++ solicitar/views.py
@@ -1,6 +1,8 @@
+# -*- encoding: utf-8 -*-
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
+from usuario.models import *
 
 from solicitar.models import *
 # Create your views here.
@@ -15,8 +17,13 @@ def reparacion(request,marca, modelo):
 	return render(request, 'reparacion.html' , {"reparaciones" : reparacion})
 @login_required()
 def solicitar(request,marca, modelo,reparacion):
-	#send_mail('Reparacion', '', 'kiipfone@gmail.com', ['kiipfone@gmail.com'], html_message='Marca: %s<br>Modelo: %s<br>Reparacion: %s' %(marca,modelo,reparacion))
-	modelo = Modelo.objects.get(url=modelo)
-	reparacion = Reparacion.objects.get(url=reparacion)
-	color = modelo.color.all()
-	return render(request, 'solicitar.html' , {"modelo":modelo,"reparacion":reparacion,"colores" : color})
+	if request.method == 'POST':
+		usuario=Usuario.objects.get(user=request.user)
+		reparacion =Reparacion.objects.get(url=reparacion)
+		send_mail('Reparacion', '', 'kiipfone@gmail.com', ['kiipfone@gmail.com'], html_message='<h1>Informacion del telefono</h1>Marca: %s<br>Modelo: %s<br>Reparacion: %s<br><h1>Informacion Personal</h1>Nombre: %s <br>Apellido: %s<h1>Contacto</h1> Direccion: %s<br> Ciudad: %s<br>Pais: %s<br>Telefono: %s <br>Telefono adicional: %s<br>E-Mail: %s<h1>Informacion adicional: </h1> %s' %(marca,modelo,reparacion.nombre,request.user.first_name,request.user.last_name,request.POST.get('direccion'),request.POST.get('ciudad'),request.POST.get('pais'),usuario.celular,request.POST.get('telefono'),request.user.email,request.POST.get('observaciones')))
+		return render(request, 'home.html')
+	else:
+		modelo = Modelo.objects.get(url=modelo)
+		reparacion = Reparacion.objects.get(url=reparacion)
+		color = modelo.color.all()
+		return render(request, 'solicitar.html' , {"modelo":modelo,"reparacion":reparacion,"colores" : color})
